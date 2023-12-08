25399c9d1074d7706ba245a4862cc124d40d6095
tuanna <tuanna@volio.vn>
2023-12-08 09:50:03 +0700

    Initial Commit

---
+++ ChristmasTreeSwiftUI/ChristmasTreeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChristmasTreeSwiftUIApp.swift
+//  ChristmasTreeSwiftUI
+//
+//  Created by Nguyễn Linh on 08/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChristmasTreeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
